92d87e2fb14c7d196ad4d2fab5a9efd52279a8fc
Farooq-Asif <chsaab983@gmail.com>
2024-10-19 14:51:41 +0500

    i made some changes

--- app.js
+++ app.js
@@ -1,37 +1,37 @@
 require("dotenv").config()
-import cors from "cors";
+const cors= require("cors")
 
-const  express=require('express')
+const express = require('express')
 
-const app= express()
+const app = express()
 
 const apiUrl = process.env.REACT_APP_API_URLT || 5000;
 
-const connectDB=require('./db/connect')
-const corsOrigin ={
-    origin:'http://localhost:3000', 
-    credentials:true,            
-    optionSuccessStatus:200
+const connectDB = require('./db/connect')
+const corsOrigin = {
+    origin: 'http://localhost:3000',
+    credentials: true,
+    optionSuccessStatus: 200
 }
 app.use(cors(corsOrigin));
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.send("Hi, i am live")
 })
 
-const products_routes=require("./routes/products")
-app.use("/api/products",products_routes)
+const products_routes = require("./routes/products")
+app.use("/api/products", products_routes)
 
-const start=async()=>{
-try{
-    await connectDB(process.env.MONGODB_URL)
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGODB_URL)
 
-app.listen(apiUrl,()=>{
-    console.log(`${apiUrl} Yes I am connected`)
-})
-}
-catch(error){
-console.log(error);
+        app.listen(apiUrl, () => {
+            console.log(`${apiUrl} Yes I am connected`)
+        })
+    }
+    catch (error) {
+        console.log(error);
 
-}
+    }
 }
 start()
