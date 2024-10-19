b39787da4361e7ad32305c764d030acff3d90cbf
Farooq-Asif <chsaab983@gmail.com>
2024-10-19 14:47:07 +0500

    changes i made

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 require("dotenv").config()
+import cors from "cors";
+
 const  express=require('express')
+
 const app= express()
 
 const apiUrl = process.env.REACT_APP_API_URLT || 5000;
 
 const connectDB=require('./db/connect')
-
+const corsOrigin ={
+    origin:'http://localhost:3000', 
+    credentials:true,            
+    optionSuccessStatus:200
+}
+app.use(cors(corsOrigin));
 app.get("/",(req,res)=>{
     res.send("Hi, i am live")
 })
