396cfbe7a076d3d3cb040a51d9b0f41eac31f105
Farooq-Asif <chsaab983@gmail.com>
2024-10-21 00:40:05 +0500

    changes

--- controllers/signupController.js
+++ controllers/signupController.js
@@ -30,7 +30,7 @@ const handleSignUpSubmission = async (req, res) => {
 
         await newUser.save();
 
-        res.status(201).json({
+        res.status(200).json({
             message: 'User created successfully',
             data: { name, email, Token },
         });
